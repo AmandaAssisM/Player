ecd5bfcea4ac3cadf9c9232b7bde729fdbed7c07
Amanda Assis <assisamanda350@gmail.com>
2021-11-24 21:18:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from random import choice
 
 def init():
     player.play_music()
-
+    player.mostrar_nome_musica()
+    
     while True:
         while True:
             try:
                 opcao = player.pegar_opcao_menu()
                 player.verificar_opcao(opcao)
+                player.mostrar_nome_musica()
                 
             except SystemExit:
                 print('Saindo...')
